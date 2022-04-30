20a94a720b3afb7c1d60624c93f9d2b8f92e05a8
dwiayunovitasari <dwi.ayu.novitasari19@mhs.ubharajaya.ac.id>
2022-04-30 10:30:42 +0700

    penambahan composer

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,9 @@ Route::prefix('admin')->group(function () {
 
     Route::get('/jsIntroduction', 'JsintroController@index')->name('jsintro');
     Route::get('/jsIntroduction_next', 'JsintroController@index_next')->name('jsintro_next');
+
+    Route::get('/jsdwi', 'DwiController@index')->name('jsdwi');
+    Route::get('/jsIntroduction_next', 'JsintroController@index_next')->name('jsintro_next');
   
 
 });
